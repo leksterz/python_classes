6e6879382aab722c2068d1febf42075e92fa4aad
Alex Ablazevics <ablazevix@gmail.com>
2022-08-20 22:45:35 -0400

    basic refactoring, working on email tomorrow

--- request.py
+++ request.py
@@ -2,7 +2,7 @@ class Requests:
     """The request class manages the submition of requests via
     the initializition. It keeps track of pending requests, status = open.
 
-    The methods in this class deals with accepting a request, which 
+    The methods in this class deals with accepting a request, which
     iniatiates the cancelition of requests that shared the time slot.
 
     The other major method deals with informing the users affect by acepting
@@ -21,6 +21,25 @@ class Requests:
     def submit_req(self, schedule):
         schedule[self.date]['pending'].append(
             [self.user_info, self.time_slots])
+
+    def clear_pending(self, schedule):
+        for date in schedule.keys():
+            if (date == self.date):
+                # check the
+                for time_slot in self.time_slots:
+                    for pending_req in schedule[self.date]['pending'][:]:
+                        for pending_req_t_slot in pending_req[1][:]:
+                            print(
+                                f'\nchecking {pending_req_t_slot} from {pending_req[0]} vs {time_slot}')
+                            if (time_slot == pending_req_t_slot):
+                                schedule[self.date]['pending'].remove(
+                                    pending_req)
+                                # the print statement below is the email func
+                                print(
+                                    f'removing {pending_req[0]}')
+                                break
+                            else:
+                                print('no match, keep')
 
     def accept_req(self, schedule):
         # flag keeps track of whether a req was accepted
@@ -41,26 +60,7 @@ class Requests:
         if (req_accepted == True):
             # we want to compare the accepted_req time slots
             # with each of the time_slots for a request
-            for date in schedule.keys():
-                if (date == self.date):
-                    # check the
-                    for time_slot in self.time_slots:
-                        for pending_req in schedule[self.date]['pending'][:]:
-                            for pending_req_t_slot in pending_req[1][:]:
-                                print(
-                                    f'\nchecking {pending_req_t_slot} from {pending_req[0]} vs {time_slot}')
-                                if (time_slot == pending_req_t_slot):
-                                    schedule[self.date]['pending'].remove(
-                                        pending_req)
-                                    print(
-                                        f'removing {pending_req[0]}')
-                                    break
-                                else:
-                                    print('no match, keep')
-
-            # notify user if their spot is canceled
-
-        # notify user who's spot was accepted
+            self.clear_pending(schedule)
 
     def __repr_(self):
         return self.user_info, self.date, self.time_slots
